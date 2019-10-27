9e818819a2a8906d888f6cad075c33c8efdc2eca
Alejandro Roa <a.roa@fixme.es>
2019-10-27 08:48:47 -0500

    tutorial1

--- src/app/client-list/client-list.component.ts
+++ src/app/client-list/client-list.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable } from "rxjs";
-import { Store, select } from '@ngrx/store';
-import { increment, decrement, reset } from '../counter.actions';
 
 import { ClientService } from '../client.service';
 import { ClientInterface } from '../interfaces/client';
 
-interface AppState {
-  message: string;
-}
 
 @Component({
   selector: 'app-client-list',
@@ -19,53 +13,14 @@ interface AppState {
 export class ClientListComponent implements OnInit {
   clients: Observable<ClientInterface[]>;
   
-  /*todos los objetos que vienen por store son observables*/
-  count$: Observable<number>;
-  message$: Observable<any>;
-
   constructor(
-    private store: Store<{ count: number }>,
     private clientService: ClientService,
-    private storeMessage: Store<AppState>,
-    private router: Router
   ) { 
-    this.count$ = store.pipe(select('count'));
-    this.message$ = this.storeMessage.select('message')
   }
 
   ngOnInit() {
     this.reloadData();
   }
-
-  handleSearch(value: string){
-    console.log(value)
-  }
-
-  spanishMessage(){
-    this.storeMessage.dispatch({
-      type: 'ESPAÑOL'
-    })
-  }
-
-  frenchMessage(){
-    this.storeMessage.dispatch({
-      type: 'FRANCES'
-    })
-  }
-
-  /**** STORE ****/
-  increment() {
-    this.store.dispatch(increment());
-  }
- 
-  decrement() {
-    this.store.dispatch(decrement());
-  }
- 
-  reset() {
-    this.store.dispatch(reset());
-  }
-  /**** END STORE ****/
 
   reloadData() {
     this.clients = this.clientService.getClients()
@@ -82,6 +37,5 @@ export class ClientListComponent implements OnInit {
         },
         error => console.log(error));
   }
-
 
 }
